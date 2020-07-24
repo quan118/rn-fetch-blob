a8cedac5f1eebde98961e70a7d1cfc7534a3df76
Quan Nguyen <quannt89@gmail.com>
2020-07-24 16:38:12 +0700

    fix bug: writeFile return success after write 0 byte

--- ios/RNFetchBlobFS.m
+++ ios/RNFetchBlobFS.m
@@ -376,7 +376,7 @@ NSMutableDictionary *fileStreams = nil;
         NSFileHandle *fileHandle = [NSFileHandle fileHandleForWritingAtPath:path];
         NSData * content = nil;
         if([encoding RNFBContainsString:@"base64"]) {
-            content = [[NSData alloc] initWithBase64EncodedString:data options:0];
+            content = [[NSData alloc] initWithBase64EncodedString:data options:NSDataBase64DecodingIgnoreUnknownCharacters];
         }
         else if([encoding isEqualToString:@"uri"]) {
             NSNumber* size = [[self class] writeFileFromFile:data toFile:path append:append callback:^(NSString *errMsg, NSNumber *size) {
